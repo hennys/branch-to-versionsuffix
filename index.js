c2259a63716cef2d1e4bdc4209d3ca01c30f1478
Henrik Nystrom <henrik.nystrom@episerver.com>
2023-08-29 17:55:46 +1200

    payload debug

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ try {
 
   core.setOutput("versionSuffix", versionSuffix);
 
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
